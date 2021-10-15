efaa2073f3440e208292acb8cd704cdf1f66af0e
Melany1906 <89593986+Melany1906@users.noreply.github.com>
2021-10-15 17:23:50 -0500

    Add files via upload

---
+++ Informatica 001/funciones.js
@@ -0,0 +1,67 @@
+document.write("Actividad números binarios <br>");
+document.write("Convertir los números binarios de 0000 a 1111<br>");
+var Respuesta;
+document.write("<br><br><hr>");
+Respuesta=0*Math.pow(2, 3)+ 0*Math.pow(2, 2)+ 0*Math.pow(2, 1)+ 0*Math.pow(2, 0);
+document.write("La respuesta de 0000 es:"+Respuesta+"<br>");
+document.write("<br><br><hr>");
+//============================================================================================
+Respuesta=0*Math.pow(2, 3)+ 0*Math.pow(2, 2)+ 0*Math.pow(2, 1)+ 1*Math.pow(2, 0);
+document.write("La respuesta de 0001 es:"+Respuesta+"<br>");
+document.write("<br><br><hr>");
+//============================================================================================
+Respuesta=0*Math.pow(2, 3)+ 0*Math.pow(2, 2)+ 1*Math.pow(2, 1)+ 0*Math.pow(2, 0);
+document.write("La respuesta de 0010 es:"+Respuesta+"<br>");
+document.write("<br><br><hr>");
+//============================================================================================
+Respuesta=0*Math.pow(2, 3)+ 0*Math.pow(2, 2)+ 1*Math.pow(2, 1)+ 1*Math.pow(2, 0);
+document.write("La respuesta de 0011 es:"+Respuesta+"<br>");
+document.write("<br><br><hr>");
+//============================================================================================
+Respuesta=0*Math.pow(2, 3)+ 1*Math.pow(2, 2)+ 0*Math.pow(2, 1)+ 0*Math.pow(2, 0);
+document.write("La respuesta de 0100 es:"+Respuesta+"<br>");
+document.write("<br><br><hr>");
+//============================================================================================
+Respuesta=0*Math.pow(2, 3)+ 1*Math.pow(2, 2)+ 0*Math.pow(2, 1)+ 1*Math.pow(2, 0);
+document.write("La respuesta de 0101 es:"+Respuesta+"<br>");
+document.write("<br><br><hr>");
+//============================================================================================
+Respuesta=0*Math.pow(2, 3)+ 1*Math.pow(2, 2)+ 1*Math.pow(2, 1)+ 0*Math.pow(2, 0);
+document.write("La respuesta de 0110 es:"+Respuesta+"<br>");
+document.write("<br><br><hr>");
+//============================================================================================
+Respuesta=0*Math.pow(2, 3)+ 1*Math.pow(2, 2)+ 1*Math.pow(2, 1)+ 1*Math.pow(2, 0);
+document.write("La respuesta de 0111 es:"+Respuesta+"<br>");
+document.write("<br><br><hr>");
+//============================================================================================
+Respuesta=1*Math.pow(2, 3)+ 0*Math.pow(2, 2)+ 0*Math.pow(2, 1)+ 0*Math.pow(2, 0);
+document.write("La respuesta de 1000 es:"+Respuesta+"<br>");
+document.write("<br><br><hr>");
+//============================================================================================
+Respuesta=1*Math.pow(2, 3)+ 0*Math.pow(2, 2)+ 0*Math.pow(2, 1)+ 1*Math.pow(2, 0);
+document.write("La respuesta de 1001 es:"+Respuesta+"<br>");
+document.write("<br><br><hr>");
+//============================================================================================
+Respuesta=1*Math.pow(2, 3)+ 0*Math.pow(2, 2)+ 1*Math.pow(2, 1)+ 0*Math.pow(2, 0);
+document.write("La respuesta de 1010 es:"+Respuesta+"<br>");
+document.write("<br><br><hr>");
+//============================================================================================
+Respuesta=1*Math.pow(2, 3)+ 0*Math.pow(2, 2)+ 1*Math.pow(2, 1)+ 1*Math.pow(2, 0);
+document.write("La respuesta de 1011 es:"+Respuesta+"<br>");
+document.write("<br><br><hr>");
+//============================================================================================
+Respuesta=1*Math.pow(2, 3)+ 1*Math.pow(2, 2)+ 0*Math.pow(2, 1)+ 0*Math.pow(2, 0);
+document.write("La respuesta de 1100 es:"+Respuesta+"<br>");
+document.write("<br><br><hr>");
+//============================================================================================
+Respuesta=1*Math.pow(2, 3)+ 1*Math.pow(2, 2)+ 0*Math.pow(2, 1)+ 1*Math.pow(2, 0);
+document.write("La respuesta de 1101 es:"+Respuesta+"<br>");
+document.write("<br><br><hr>");
+//============================================================================================
+Respuesta=1*Math.pow(2, 3)+ 1*Math.pow(2, 2)+ 1*Math.pow(2, 1)+ 0*Math.pow(2, 0);
+document.write("La respuesta de 1110 es:"+Respuesta+"<br>");
+document.write("<br><br><hr>");
+//============================================================================================
+Respuesta=1*Math.pow(2, 3)+ 1*Math.pow(2, 2)+ 1*Math.pow(2, 1)+ 1*Math.pow(2, 0);
+document.write("La respuesta de 1111 es:"+Respuesta+"<br>");
+document.write("<br><br><hr>");
